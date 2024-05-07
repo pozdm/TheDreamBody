add95d24e4c63326b6c49fe55425f80ff4520578
Michael <pozdinmisha666@gmail.com>
2024-05-08 01:28:59 +0300

    FEAT/добавлены конверторы в блок маршрутизации

--- thedreambody/exercises/views.py
+++ thedreambody/exercises/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('страница приложения the dream body')
+    return HttpResponse('<h1>Страница приложения the dream body</h1>')
 
 
-def categories(request):
-    return HttpResponse('упражнения на группы мышц')
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Упражнения на группы мышц</h1><p>id: {cat_id}</p>')
 
+
+def categories_by_slug(reguest, cat_slug):
+    return HttpResponse(f'<h1>Упражнения на группы мышц</h1><p>slug: {cat_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
